c54da275808713b572360f43c1af6a10ea4114ef
iovigor <iovigor@yahoo.com>
2024-02-09 21:45:55 +0200

    Work with import

---
+++ src/Lesson10/p1/A.java
@@ -0,0 +1,4 @@
+package Lesson10.p1;
+
+public class A {
+}
---
+++ src/Lesson10/p4/D.java
@@ -0,0 +1,23 @@
+package Lesson10.p4;
+
+import Lesson10.p1.A;
+import Lesson10.p1.p2.p3.C;
+import Lesson10.p4.p5.E;
+import org.w3c.dom.ls.LSOutput;
+
+import static Lesson10.p1.p2.B.*;
+
+public class D {
+
+    public static void main(String[] args) {
+        A a1 = new A();
+        C c1 = new C();
+        E e1 = new E();
+        System.out.println(a1);
+        System.out.println(c1);
+        System.out.println(e1);
+        System.out.println(b1);
+        System.out.println(b2);
+    }
+
+}
